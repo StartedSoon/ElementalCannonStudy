9c1868f6d9a0d2da89912484b24aa91315745421
HP <HP@DESKTOP-UCDQ0S1>
2023-10-27 00:22:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string elementDurumu;
+for (int sayac1 = 1; sayac1 <= 100; sayac1++)
+{
+    if (sayac1 % 15 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        elementDurumu = "Özel";
+    }
+    else if(sayac1 % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        elementDurumu = "Elektrik";
+    }
+    else if(sayac1 % 3 == 0)
+    {
+        Console.ForegroundColor= ConsoleColor.Red;
+        elementDurumu = "Ateş";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        elementDurumu = "Normal";
+    }
+    Console.WriteLine($"{sayac1}: {elementDurumu}");
+}
+
